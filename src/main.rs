4c2c71c4e32cd1270d278ea05992c3d6aece8fa4
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-02 09:44:00 +0100

    day 02 #1

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,43 @@ fn main() {
 }
 
 fn execute(input: String) -> usize {
-    1
+    input
+        .split("\n")
+        .map(|s| s.split(" ").collect::<Vec<&str>>())
+        .filter(|values| values.len() == 2)
+        .map(|values| score(values[1], values[0]))
+        .sum::<i32>() as usize
+}
+
+// So ugly! :D
+fn score(me: &str, opponent: &str) -> i32 {
+    if me == "X" && opponent == "A" {
+        1 + 3
+    } else if me == "Y" && opponent == "B" {
+        2 + 3
+    } else if me == "Z" && opponent == "C" {
+        3 + 3
+    } else if me == "X" && opponent == "B" {
+        1 + 0
+    } else if me == "X" && opponent == "C" {
+        1 + 6
+    } else if me == "Y" && opponent == "A" {
+        2 + 6
+    } else if me == "Y" && opponent == "C" {
+        2 + 0
+    } else if me == "Z" && opponent == "A" {
+        3 + 0
+    } else if me == "Z" && opponent == "B" {
+        3 + 6
+    } else { 0 }
 }
 
 #[test]
 fn test_data() {
     assert_eq!(execute(r"
-".to_string()), 0);
+A Y
+B X
+C Z
+".to_string()), 15);
 }
 
