dc99de50c6cfd9925c8dee41a3e8ca5a5243d7fe
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-05 13:53:35 +0100

    refactor, still as obfuscated...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::fs;
+use std::str::FromStr;
 use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
     static ref MOVE_REGEX: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+}
+
+type Board = Vec<Vec<char>>;
+
+struct Move {
+    from: u8,
+    to: u8,
+    count: u8,
 }
 
 fn main() {
@@ -13,59 +22,73 @@ fn main() {
 
 fn execute(input: String) -> String {
     let parts: Vec<&str> = input.split("\n\n").collect();
-
     let board = parts[1].lines()
-        .map(|l| parse_move(l))
-        .fold(parse_board(parts[0]), |board: Vec<Vec<char>>, (count, from, to)| {
-            handle_move(board, count, from, to)
-        });
+        .filter_map(|s| s.parse::<Move>().ok())
+        .fold(parse_board(parts[0]), |board: Board, m| m.handle_move(board));
+
     board.iter()
         .filter_map(|s| s.last())
         .collect()
 }
 
-fn handle_move(mut board: Vec<Vec<char>>, count: u8, from: u8, to: u8) -> Vec<Vec<char>> {
-    let mut tmp = vec![];
-    (1..count + 1).for_each(|_| {
-        let x = board[from as usize - 1].pop().unwrap();
-        tmp.push(x)
-    });
-    (1..count + 1).for_each(|_| {
-        let x = tmp.pop().unwrap();
-        board[to as usize - 1].push(x);
-    });
+impl Move {
+    fn handle_move(&self, mut board: Board) -> Board {
+        let mut tmp = vec![];
+        (1..self.count + 1).for_each(|_| {
+            let x = board[self.from as usize - 1].pop().unwrap();
+            tmp.push(x)
+        });
+        (1..self.count + 1).for_each(|_| {
+            let x = tmp.pop().unwrap();
+            board[self.to as usize - 1].push(x);
+        });
+        board
+    }
+}
+
+impl FromStr for Move {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let captures = MOVE_REGEX.captures(s).unwrap();
+        Ok(Move {
+            count: captures[1].parse::<u8>().unwrap(),
+            from: captures[2].parse::<u8>().unwrap(),
+            to: captures[3].parse::<u8>().unwrap(),
+        })
+    }
+}
+
+fn parse_board(input: &str) -> Board {
+    input.lines()
+        .rev()
+        .map(|l| get_crates_on_line(l))
+        .fold(empty_board(), |board: Board, crates| push_crates_on_board(board, crates))
+}
+
+fn push_crates_on_board(mut board: Board, crates: Vec<Option<char>>) -> Board {
+    (0..9).into_iter()
+        .for_each(|i| {
+            if let Some(c) = crates[i] {
+                board[i].push(c);
+            }
+        });
     board
 }
 
-fn parse_board(input: &str) -> Vec<Vec<char>> {
-    input.lines()
-        .map(|l| (0..9).into_iter()
-            .map(|i| get_element_at(l, i))
-            .collect::<Vec<Option<char>>>())
-        .rev()
-        .fold(empty_game(), |mut acc: Vec<Vec<char>>, chars| {
-            (0..9).into_iter()
-                .for_each(|i| {
-                    if let Some(c) = chars[i] {
-                        acc[i].push(c);
-                    }
-                });
-            acc
-        })
+fn get_crates_on_line(l: &str) -> Vec<Option<char>> {
+    (0..9).into_iter()
+        .map(|i| get_crate_at(l, i))
+        .collect::<Vec<Option<char>>>()
 }
 
-fn empty_game() -> Vec<Vec<char>> {
+fn empty_board() -> Board {
     (0..9).into_iter()
         .map(|_| vec![])
         .collect()
 }
 
-fn parse_move(l: &str) -> (u8, u8, u8) {
-    let captures = MOVE_REGEX.captures(l).unwrap();
-    (captures[1].parse::<u8>().unwrap(), captures[2].parse::<u8>().unwrap(), captures[3].parse::<u8>().unwrap())
-}
-
-fn get_element_at(l: &str, index: usize) -> Option<char> {
+fn get_crate_at(l: &str, index: usize) -> Option<char> {
     if l.len() >= 1 + index * 4 {
         let res = l.as_bytes()[1 + index * 4] as char;
         if res == ' ' {
