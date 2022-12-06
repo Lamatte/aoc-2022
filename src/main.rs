608a62407dbb8e6598a74b78959e6b6260b42535
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-06 10:08:09 +0100

    day 06 #1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::VecDeque;
 use std::fs;
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("resources/input.txt").expect("Could not read file");
@@ -6,12 +8,24 @@ fn main() {
 }
 
 fn execute(input: String) -> usize {
-    1
+    let mut count = 0;
+    let mut last_chars: VecDeque<u8> = VecDeque::new();
+    for c in input.as_bytes() {
+        count = count + 1;
+        if last_chars.len() >= 4 {
+            last_chars.pop_front();
+        }
+        last_chars.push_back(*c);
+        if last_chars.iter().unique().count() == 4 {
+            return count;
+        }
+    }
+    0
 }
 
 #[test]
 fn test_data() {
-    assert_eq!(execute(r"
-".to_string()), 0);
+    assert_eq!(execute(r"mjqjpqmgbljsphdztnvjfqwrcgsmlb
+".to_string()), 7);
 }
 
