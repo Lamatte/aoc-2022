d09788cc98b90c49665f52d218255e0ba789b84c
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-08 16:38:54 +0100

    refactor: better??

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::fs;
+use std::str::FromStr;
 use std::time::Instant;
 use itertools::Itertools;
+
+type Coordinates = (usize, usize);
+
+struct Grid {
+    raw: Vec<Vec<u8>>,
+}
 
 fn main() {
     let input = fs::read_to_string("resources/input.txt").expect("Could not read file");
@@ -11,64 +18,77 @@ fn main() {
 }
 
 fn execute(input: String) -> usize {
-    let grid = parse_input(input);
-    (0..grid.len())
-        .flat_map(|line| (0..grid.len()).map(move |column| (line, column)))
-        .map(|(line, column)| scenic_score(&grid, line, column))
+    let grid = input.parse::<Grid>().unwrap();
+    grid.all_coordinates().iter()
+        .map(|&coordinates| grid.scenic_score(coordinates))
         .sorted()
         .last().unwrap()
 }
 
-fn scenic_score(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> usize {
-    visible_left_count(grid, line, column) * visible_right_count(grid, line, column) * visible_top_count(grid, line, column) * visible_bottom_count(grid, line, column)
-}
-
-fn visible_left_count(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> usize {
-    count(
-        (0..column).rev().map(|column| grid[line][column]).collect(),
-        grid[line][column],
-    )
-}
-
-fn visible_top_count(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> usize {
-    count(
-        (0..line).rev().map(|line| grid[line][column]).collect(),
-        grid[line][column],
-    )
-}
-
-fn visible_right_count(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> usize {
-    count(
-        (column + 1..grid.len()).map(|column| grid[line][column]).collect(),
-        grid[line][column],
-    )
-}
-
-fn visible_bottom_count(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> usize {
-    count(
-        (line + 1..grid.len()).map(|line| grid[line][column]).collect(),
-        grid[line][column],
-    )
-}
-
-fn count(values: Vec<u8>, current_value: u8) -> usize {
-    let mut res = 0;
-    for value in values {
-        res = res + 1;
-        if value >= current_value {
-            break;
-        }
+impl Grid {
+    fn scenic_score(&self, coordinates: Coordinates) -> usize {
+        self.visible_left_count(coordinates) * self.visible_right_count(coordinates) * self.visible_top_count(coordinates) * self.visible_bottom_count(coordinates)
     }
-    res
+
+    fn size(&self) -> usize {
+        self.raw.len()
+    }
+
+    fn all_coordinates(&self) -> Vec<Coordinates> {
+        (0..self.size()).flat_map(|line| (0..self.size()).map(move |column| (line, column))).collect::<Vec<Coordinates>>()
+    }
+
+    fn visible_left_count(&self, coordinates: Coordinates) -> usize {
+        Self::count(
+            (0..coordinates.1).rev().map(|column| self.raw[coordinates.0][column]).collect(),
+            self.raw[coordinates.0][coordinates.1],
+        )
+    }
+
+    fn visible_top_count(&self, coordinates: Coordinates) -> usize {
+        Self::count(
+            (0..coordinates.0).rev().map(|line| self.raw[line][coordinates.1]).collect(),
+            self.raw[coordinates.0][coordinates.1],
+        )
+    }
+
+    fn visible_right_count(&self, coordinates: Coordinates) -> usize {
+        Self::count(
+            (coordinates.1 + 1..self.size()).map(|column| self.raw[coordinates.0][column]).collect(),
+            self.raw[coordinates.0][coordinates.1],
+        )
+    }
+
+    fn visible_bottom_count(&self, coordinates: Coordinates) -> usize {
+        Self::count(
+            (coordinates.0 + 1..self.size()).map(|line| self.raw[line][coordinates.1]).collect(),
+            self.raw[coordinates.0][coordinates.1],
+        )
+    }
+
+    fn count(values: Vec<u8>, current_value: u8) -> usize {
+        let mut res = 0;
+        for value in values {
+            res = res + 1;
+            if value >= current_value {
+                break;
+            }
+        }
+        res
+    }
 }
 
-fn parse_input(input: String) -> Vec<Vec<u8>> {
-    let grid = input.lines()
-        .map(|l| l.chars()
-            .map(|c| c as u8 - 48)
-            .collect::<Vec<u8>>())
-        .collect::<Vec<Vec<u8>>>();
-    grid
+impl FromStr for Grid {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let raw = s.lines()
+            .map(|l| l.chars()
+                .map(|c| c as u8 - 48)
+                .collect::<Vec<u8>>())
+            .collect::<Vec<Vec<u8>>>();
+        Ok(Grid { raw })
+    }
 }
 
 #[test]
