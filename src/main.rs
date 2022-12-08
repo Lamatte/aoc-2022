d1df71e9c78ed4843064c59a2877357956f2863e
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-08 10:27:35 +0100

    day 08 #1

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,75 @@ fn main() {
 }
 
 fn execute(input: String) -> usize {
-    1
+    let grid = input.lines()
+        .map(|l| l.chars()
+            .map(|c| c as u8 - 48)
+            .collect::<Vec<u8>>())
+        .collect::<Vec<Vec<u8>>>();
+    let mut res = 0;
+    for line in 1..grid.len()-1 {
+        for column in 1..grid.len()-1 {
+            if is_visible(&grid, line, column) {
+                eprintln!("{}/{} is visible", line, column);
+                res = res + 1;
+            }
+        }
+    }
+    res + edges(grid)
+}
+
+fn edges(grid: Vec<Vec<u8>>) -> usize {
+    grid.len()*4-4
+}
+
+fn is_visible(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> bool {
+    is_visible_left(grid, line, column) || is_visible_right(grid, line, column) || is_visible_top(grid, line, column) || is_visible_bottom(grid, line, column)
+}
+
+fn is_visible_left(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> bool {
+    for other_column in 0..column {
+        if grid[line][other_column] >= grid[line][column] {
+            return false;
+        }
+    };
+    true
+}
+
+fn is_visible_right(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> bool {
+    for other_column in column+1..grid.len() {
+        if grid[line][other_column] >= grid[line][column] {
+            return false;
+        }
+    };
+    true
+}
+
+fn is_visible_top(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> bool {
+    for other_line in 0..line {
+        if grid[other_line][column] >= grid[line][column] {
+            return false;
+        }
+    };
+    true
+}
+
+
+fn is_visible_bottom(grid: &Vec<Vec<u8>>, line: usize, column: usize) -> bool {
+    for other_line in line+1..grid.len() {
+        if grid[other_line][column] >= grid[line][column] {
+            return false;
+        }
+    };
+    true
 }
 
 #[test]
 fn test_data() {
-    assert_eq!(execute(r"
-".to_string()), 0);
+    assert_eq!(execute(r"30373
+25512
+65332
+33549
+35390
+".to_string()), 21);
 }
 
