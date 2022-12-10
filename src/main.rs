61060fadd85aab3dad6b95ca9a2d8f127c5384d2
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-10 13:56:46 +0100

    Add bench

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ use std::time::Instant;
 
 fn main() {
     let input = fs::read_to_string("resources/input.txt").expect("Could not read file");
-    let start = Instant::now();
-    let result = execute(input);
-    eprintln!("Elapsed time: {:?}", start.elapsed());
+    let result = execute(&input);
     println!("{}", result);
+    eprintln!("Benching...");
+    let start = Instant::now();
+    for _ in 0..1000 {
+        execute(&input);
+    }
+    eprintln!("Average elapsed time: {:?}", start.elapsed()/1000);
 }
 
-fn execute(input: String) -> usize {
+fn execute(input: &String) -> usize {
     1
 }
 
 #[test]
 fn test_data() {
-    assert_eq!(execute(r"
+    assert_eq!(execute(&r"
 ".to_string()), 0);
 }
 
