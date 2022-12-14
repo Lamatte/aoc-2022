55cf87fee9b824969d7e91710c52df668d1c8aa1
Mathieu Chauvin <mathieu.chauvin@orange.com>
2022-12-14 10:49:14 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use std::cmp::{max, min};
 use std::collections::HashSet;
 use std::fs;
+use std::str::FromStr;
 use std::time::Instant;
-use itertools::{Itertools, sorted};
+use itertools::Itertools;
+
+type Position = (i32, i32); // (line, column)
+
+struct Grid {
+    cells: HashSet<Position>,
+    depth: i32,
+}
 
 fn main() {
     let input = fs::read_to_string("resources/input.txt").expect("Could not read file");
@@ -11,77 +19,71 @@ fn main() {
     eprintln!("Benching...");
     let start = Instant::now();
     for _ in 0..1000 {
-        //execute(&input);
+        execute(&input);
     }
     eprintln!("Average elapsed time: {:?}", start.elapsed() / 1000);
 }
 
 fn execute(input: &String) -> usize {
-    let mut map = input.lines()
-        .flat_map(|line| parse_path(line))
-        .collect::<HashSet<(i32, i32)>>();
-    print(&mut map);
-    let i = max_line(&mut &mut map);
+    let mut grid = input.parse::<Grid>().unwrap();
+    //grid.print();
     let mut count = 0;
     loop {
-        let position = add_sand(&mut map, i);
-        match position {
-            None => {
-                break;
-            }
-            Some(_) => {
-                count += 1;
-                //print(&mut map);
-            }
+        match grid.try_and_drop_sand_at((0, 500)) {
+            None => break,
+            Some(_) => count += 1,
         }
     }
     count
 }
 
-fn print(mut map: &mut HashSet<(i32, i32)>) {
-    (0..max_line(&mut map) + 2).for_each(|line| {
-        let line_str: String = (min_column(&mut map)-10..max_column(&mut map) + 12).map(|column| if map.contains(&(line, column)) { '#' } else { '.' }).collect();
-        eprintln!("{}", line_str);
-    });
-}
-
-fn max_column(map: &mut &mut HashSet<(i32, i32)>) -> i32 {
-    *map.iter().map(|(_, col)| col).sorted().last().unwrap()
-}
-
-fn min_column(map: &mut &mut HashSet<(i32, i32)>) -> i32 {
-    *map.iter().map(|(_, col)| col).sorted().rev().last().unwrap()
-}
-
-fn max_line(map: &mut &mut HashSet<(i32, i32)>) -> i32 {
-    *map.iter().map(|(line, _)| line).sorted().last().unwrap()
-}
-
-fn add_sand(mut map: &mut HashSet<(i32, i32)>, i: i32) -> Option<(i32, i32)> {
-    let mut sand_position = (0, 500);
-    loop {
-        if map.contains(&(sand_position.0, sand_position.1)) {
+impl Grid {
+    fn try_and_drop_sand_at(&mut self, position: (i32, i32)) -> Option<(i32, i32)> {
+        if self.cells.contains(&(position.0, position.1)) {
             return None;
         }
-        if sand_position.0 == i +1 {
-            map.insert(sand_position);
-            return Some(sand_position);
+        let mut sand_position = position;
+        loop {
+            if sand_position.0 == self.depth + 1 {
+                // Reached floor
+                break;
+            } else if !self.cells.contains(&(sand_position.0 + 1, sand_position.1)) {
+                sand_position.0 += 1;
+            } else if !self.cells.contains(&(sand_position.0 + 1, sand_position.1 - 1)) {
+                sand_position.0 += 1;
+                sand_position.1 -= 1;
+            } else if !self.cells.contains(&(sand_position.0 + 1, sand_position.1 + 1)) {
+                sand_position.0 += 1;
+                sand_position.1 += 1;
+            } else {
+                // Resting here!
+                break;
+            }
         }
-        if !map.contains(&(sand_position.0 + 1, sand_position.1)) {
-            sand_position.0 += 1;
-        } else if !map.contains(&(sand_position.0 + 1, sand_position.1 - 1)) {
-            sand_position.0 += 1;
-            sand_position.1 -= 1;
-        } else if !map.contains(&(sand_position.0 + 1, sand_position.1 + 1)) {
-            sand_position.0 += 1;
-            sand_position.1 += 1;
-        } else {
-            break;
-        }
+        self.cells.insert(sand_position);
+        Some(sand_position)
     }
-    map.insert(sand_position);
-    //eprintln!("{:?}", sand_position);
-    Some(sand_position)
+
+    fn print(&self) {
+        let min_column = min_column(&self.cells);
+        let max_column = max_column(&self.cells);
+        (0..self.depth + 2).for_each(|line| {
+            let line_str: String = (min_column - 10..max_column + 12).map(|column| if self.cells.contains(&(line, column)) { '#' } else { '.' }).collect();
+            eprintln!("{}", line_str);
+        });
+    }
+}
+
+impl FromStr for Grid {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let map = s.lines()
+            .flat_map(|line| parse_path(line))
+            .collect::<HashSet<(i32, i32)>>();
+        let depth = max_line(&map);
+        Ok(Grid { cells: map, depth })
+    }
 }
 
 fn parse_path(line: &str) -> Vec<(i32, i32)> {
@@ -93,16 +95,12 @@ fn parse_path(line: &str) -> Vec<(i32, i32)> {
         })
         .fold(vec![], |mut path, point| {
             let last_point = path.last();
-            eprintln!("drawing line from {:?} to {:?}", last_point, point);
             match last_point {
                 None => path.push(point),
                 Some(&(line, column)) => {
-                    //eprintln!("{} to {}", line, point.1);
                     if line == point.0 {
-                        eprintln!("same line");
                         (min(column, point.1)..max(column, point.1) + 1).into_iter().for_each(|column| path.push((line, column)));
                     } else {
-                        eprintln!("same col");
                         (min(line, point.0)..max(line, point.0) + 1).into_iter().for_each(|line| path.push((line, column)));
                     }
                     path.push(point);
@@ -111,6 +109,19 @@ fn parse_path(line: &str) -> Vec<(i32, i32)> {
             path
         })
 }
+
+fn max_column(map: &HashSet<(i32, i32)>) -> i32 {
+    *map.iter().map(|(_, col)| col).sorted().last().unwrap()
+}
+
+fn min_column(map: &HashSet<(i32, i32)>) -> i32 {
+    *map.iter().map(|(_, col)| col).sorted().rev().last().unwrap()
+}
+
+fn max_line(map: &HashSet<(i32, i32)>) -> i32 {
+    *map.iter().map(|(line, _)| line).sorted().last().unwrap()
+}
+
 
 #[test]
 fn test_data() {
